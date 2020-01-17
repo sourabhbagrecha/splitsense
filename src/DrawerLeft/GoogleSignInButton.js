0d8df48edfff8c0e5f8f4a89fbbdb51047ef0653
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2020-01-17 17:14:26 +0530

    Add userdetails to localStorage

--- src/DrawerLeft/GoogleSignInButton.js
+++ src/DrawerLeft/GoogleSignInButton.js
@@ -5,20 +5,21 @@ import { serverUrl } from '../constants';
 
 function GoogleSignInButton(props) {
   const { user, signOut, signInWithGoogle } = props;
-  const signIn = (e) => {
-    signInWithGoogle(e)
-    .then(response => {
-      const {profile, isNewUser} = response.additionalUserInfo;
-      return isNewUser ? Axios.post(`${serverUrl}/user/new`, {profile, isNewUser}) : null;
-    })
-    .then(_ => {
-      console.log("Connected");
-      return true;
-    })
-    .catch(console.log)
+  const signIn = async (e) => {
+    const response = await signInWithGoogle(e);
+    const {profile, isNewUser} = response.additionalUserInfo;
+    const googleId = response.user.providerData[0].uid
+    const responseId = await Axios.post(`${serverUrl}/helper/find-by-googleid`, {googleId});
+    const {id} = responseId.data;
+    window.localStorage.setItem("user", JSON.stringify({profile, isNewUser, googleId, serverId: id}))
+    return isNewUser ? Axios.post(`${serverUrl}/user/new`, {profile, isNewUser}) : null
+  };
+  const signOutHandler = async (e) => {
+    window.localStorage.setItem("user", "");
+    signOut();
   }
   return (
-    <div onClick={user ? signOut : signIn} className='g-sign-in-button'>
+    <div onClick={user ? signOutHandler : signIn} className='g-sign-in-button'>
       <div className='content-wrapper'>
         <div className='logo-wrapper'>  
           <img src={user ? 'https://lh3.googleusercontent.com/a-/AAuE7mDLLumXJIHQQg-rxxAmEvpoH70pUOxVbF5_N2mtQA' : 'https://developers.google.com/identity/images/g-logo.png'} alt='google sign in' />
